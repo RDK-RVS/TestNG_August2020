edc453b6577708f49f3a2d639e015975cc65a3c9
RDK-RVS <76233517+RDK-RVS@users.noreply.github.com>
2021-01-17 22:27:19 -0600

    2nd commit

---
+++ src/main/java/variousConcepts/TestGit3.java
@@ -0,0 +1,12 @@
+package variousConcepts;
+
+public class TestGit3 {
+
+	public static void main(String[] args) {
+		printMe();
+	}
+	
+	public static void printMe() {
+		System.out.println("Test Git");
+	}
+}
